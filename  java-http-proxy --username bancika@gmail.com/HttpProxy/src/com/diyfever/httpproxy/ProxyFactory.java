0fef9fdffac7377816924b9a7b7278d09c5529b6
Branislav Stojkovic <Branislav Stojkovic@Bancika-PC>
2016-10-28 00:32:09 +0200

    switch the code to use janino compiler

---  java-http-proxy --username bancika@gmail.com/HttpProxy/src/com/diyfever/httpproxy/ProxyFactory.java
+++  java-http-proxy --username bancika@gmail.com/HttpProxy/src/com/diyfever/httpproxy/ProxyFactory.java
@@ -7,15 +7,10 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-
-import javax.tools.Diagnostic;
-import javax.tools.DiagnosticCollector;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.ToolProvider;
 
 import org.apache.log4j.Logger;
+import org.codehaus.commons.compiler.ISimpleCompiler;
+import org.codehaus.janino.CompilerFactory;
 
 /**
  * Utility that dynamically creates web proxy objects.
@@ -25,161 +20,119 @@ import org.apache.log4j.Logger;
  */
 public class ProxyFactory {
 
-	private static final Logger LOG = Logger.getLogger(ProxyFactory.class);
+  private static final Logger LOG = Logger.getLogger(ProxyFactory.class);
 
-	private FileClassLoader fileClassLoader;
-	private JavaCompiler compiler;
-	private IFlatProxy flatProxy;
+  private IFlatProxy flatProxy;
 
-	/**
-	 * Creates a new instance of {@link ProxyFactory} for the specified
-	 * {@link IFlatProxy}. All server calls will be delegated to the specified
-	 * {@link IFlatProxy}.
-	 * 
-	 * @param flatProxy
-	 */
-	public ProxyFactory(IFlatProxy flatProxy) {
-		super();
-		this.flatProxy = flatProxy;
+  /**
+   * Creates a new instance of {@link ProxyFactory} for the specified {@link IFlatProxy}. All server
+   * calls will be delegated to the specified {@link IFlatProxy}.
+   * 
+   * @param flatProxy
+   */
+  public ProxyFactory(IFlatProxy flatProxy) {
+    super();
+    this.flatProxy = flatProxy;
+  }
 
-		this.fileClassLoader = new FileClassLoader(
-				ToolProvider.getSystemToolClassLoader());
-		this.compiler = ToolProvider.getSystemJavaCompiler();
-	}
+  /**
+   * Dynamically creates proxy class and instatiates it. All methods from the specified interface
+   * <code>clazz</code> are implemented in the dynamically created class. Method parameters that are
+   * annotated with {@link ParamName} annotation are named according to annotation. Parameters
+   * without the annotation are named <code>paramN</code> where <code>N</code> is parameter index.
+   * 
+   * @param <T>
+   * @param clazz
+   * @param url
+   * @return
+   */
+  @SuppressWarnings("unchecked")
+  public <T> T createProxy(Class<? extends T> clazz, String url) {
+    if (!clazz.isInterface()) {
+      LOG.error("Specified class is not an interface: " + clazz.getName());
+      return null;
+    }
+    String interfaceName = clazz.getName();
+    String newClassName = interfaceName.substring(interfaceName.lastIndexOf(".") + 1) + "Impl";
+    try {
+      LOG.info("Creating temp class code");
+      StringWriter writer = new StringWriter();
+      PrintWriter out = new PrintWriter(writer);
 
-	/**
-	 * Dynamically creates proxy class and instatiates it. All methods from the
-	 * specified interface <code>clazz</code> are implemented in the dynamically
-	 * created class. Method parameters that are annotated with
-	 * {@link ParamName} annotation are named according to annotation.
-	 * Parameters without the annotation are named <code>paramN</code> where
-	 * <code>N</code> is parameter index.
-	 * 
-	 * @param <T>
-	 * @param clazz
-	 * @param url
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> T createProxy(Class<? extends T> clazz, String url) {
-		if (!clazz.isInterface()) {
-			LOG.error("Specified class is not an interface: " + clazz.getName());
-			return null;
-		}
-		String interfaceName = clazz.getName();
-		String newClassName = interfaceName.substring(interfaceName
-				.lastIndexOf(".") + 1) + "Impl";
-		try {
-			LOG.info("Creating temp class code");
-			StringWriter writer = new StringWriter();
-			PrintWriter out = new PrintWriter(writer);
+      out.println("public class " + newClassName + " implements " + interfaceName + " {\n");
+      out.println("  private " + IFlatProxy.class.getName() + " proxy;\n");
+      // Generate a constructor
+      out.println("  public " + newClassName + "(" + IFlatProxy.class.getName() + " proxy) { this.proxy = proxy; }\n");
+      for (Method method : clazz.getDeclaredMethods()) {
+        out.print("  public " + method.getReturnType().getName() + " " + method.getName() + "(");
+        for (int i = 0; i < method.getParameterTypes().length; i++) {
+          if (i > 0) {
+            out.print(", ");
+          }
+          out.print(method.getParameterTypes()[i].getName() + " " + extractParameterName(method, i));
+        }
+        out.print(") {\n");
 
-			out.println("public class " + newClassName + " implements "
-					+ interfaceName + " {\n");
-			out.println("  private " + IFlatProxy.class.getName() + " proxy;\n");
-			// Generate a constructor
-			out.println("  public " + newClassName + "("
-					+ IFlatProxy.class.getName()
-					+ " proxy) { this.proxy = proxy; }\n");
-			for (Method method : clazz.getDeclaredMethods()) {
-				out.print("  public " + method.getReturnType().getName() + " "
-						+ method.getName() + "(");
-				for (int i = 0; i < method.getParameterTypes().length; i++) {
-					if (i > 0) {
-						out.print(", ");
-					}
-					out.print(method.getParameterTypes()[i].getName() + " "
-							+ extractParameterName(method, i));
-				}
-				out.print(") {\n");
+        // Put together the parameter map.
+        out.println("    java.util.Map<String, Object> params = new java.util.HashMap<String, Object>();");
+        for (int i = 0; i < method.getParameterTypes().length; i++) {
+          String paramName = extractParameterName(method, i);
+          out.println("    params.put(\"" + paramName + "\", " + paramName + ");");
+        }
 
-				// Put together the parameter map.
-				out.println("    java.util.Map<String, Object> params = new java.util.HashMap<String, Object>();");
-				for (int i = 0; i < method.getParameterTypes().length; i++) {
-					String paramName = extractParameterName(method, i);
-					out.println("    params.put(\"" + paramName + "\", "
-							+ paramName + ");");
-				}
+        // Call the proxy
+        String factoryMethodName;
+        if (InputStream.class.equals(method.getReturnType())) {
+          factoryMethodName = "invoke";
+        } else {
+          factoryMethodName = "invokeAndDeserialize";
+        }
+        out.println("    return (" + method.getReturnType().getName() + ") proxy." + factoryMethodName + "(\"" + url
+            + "\", \"" + method.getName() + "\", params);");
+        out.println("  }\n");
+      }
+      out.print("}\n");
 
-				// Call the proxy
-				String factoryMethodName;
-				if (InputStream.class.equals(method.getReturnType())) {
-					factoryMethodName = "invoke";
-				} else {
-					factoryMethodName = "invokeAndDeserialize";
-				}
-				out.println("    return (" + method.getReturnType().getName()
-						+ ") proxy." + factoryMethodName + "(\"" + url
-						+ "\", \"" + method.getName() + "\", params);");
-				out.println("  }\n");
-			}
-			out.print("}\n");
+      String code = writer.toString();
 
-			String code = writer.toString();
-//			LOG.debug("Generated code:\n" + code);
-			JavaFileObject file = new JavaStringSource(newClassName, code);
+      ISimpleCompiler compiler = new CompilerFactory().newSimpleCompiler();
+      compiler.cook(code);
+      T instance = (T) compiler.getClassLoader().loadClass(newClassName).getConstructors()[0].newInstance(flatProxy);
+    
+      LOG.info("Successfully instantiated proxy");
+      return instance;
+    } catch (Exception e) {
+      e.printStackTrace();
+      LOG.error("Could not create proxy: " + e.getMessage());
+    }
+    return null;
+  }
 
-			LOG.info("Compiling temp class");
-			DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
-			Iterable<? extends JavaFileObject> compilationUnits = Arrays
-					.asList(file);
-			 
-			File tempDir = createTempDirectory();
-	
-			JavaCompiler.CompilationTask task = compiler.getTask(null, null,
-					diagnostics, Arrays.asList(new String[] { "-d", tempDir.getAbsolutePath() }),
-					null, compilationUnits);
-			boolean success = task.call();
-			if (success) {
-				LOG.info("Successfully compiled code");
-			} else {
-				LOG.error("Could not compile temp class:");
-				for (Diagnostic<?> d : diagnostics.getDiagnostics()) {
-					LOG.error(d.getLineNumber() + " : " + d.getMessage(null));
-				}
-				return null;
-			}
+  private String extractParameterName(Method method, int parameterIndex) {
+    Annotation[] annotations = method.getParameterAnnotations()[parameterIndex];
+    for (Annotation annotation : annotations) {
+      if (annotation instanceof ParamName) {
+        return ((ParamName) annotation).value();
+      }
+    }
+    LOG.warn("@" + ParamName.class.getSimpleName() + " annotation not present for method " + method.getName()
+        + ", at index " + parameterIndex);
+    return "param" + parameterIndex;
+  }
 
-			T instance = (T) fileClassLoader.createClass(
-					new File(tempDir.getAbsolutePath() + "/" + newClassName + ".class"))
-					.getConstructors()[0].newInstance(flatProxy);
-			LOG.info("Successfully instantiated proxy");
-			return instance;
-		} catch (Exception e) {
-			e.printStackTrace();
-			LOG.error("Could not create proxy: " + e.getMessage());
-		}
-		return null;
-	}
+  public static File createTempDirectory() throws IOException {
+    final File temp;
 
-	private String extractParameterName(Method method, int parameterIndex) {
-		Annotation[] annotations = method.getParameterAnnotations()[parameterIndex];
-		for (Annotation annotation : annotations) {
-			if (annotation instanceof ParamName) {
-				return ((ParamName) annotation).value();
-			}
-		}
-		LOG.warn("@" + ParamName.class.getSimpleName()
-				+ " annotation not present for method " + method.getName()
-				+ ", at index " + parameterIndex);
-		return "param" + parameterIndex;
-	}
+    temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
 
-	public static File createTempDirectory() throws IOException {
-		final File temp;
+    if (!(temp.delete())) {
+      throw new IOException("Could not delete temp file: " + temp.getAbsolutePath());
+    }
 
-		temp = File.createTempFile("temp", Long.toString(System.nanoTime()));
+    if (!(temp.mkdir())) {
+      throw new IOException("Could not create temp directory: " + temp.getAbsolutePath());
+    }
 
-		if (!(temp.delete())) {
-			throw new IOException("Could not delete temp file: "
-					+ temp.getAbsolutePath());
-		}
-
-		if (!(temp.mkdir())) {
-			throw new IOException("Could not create temp directory: "
-					+ temp.getAbsolutePath());
-		}
-
-		return (temp);
-	}
+    return (temp);
+  }
 }
