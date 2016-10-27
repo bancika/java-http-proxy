4d18624a96c12ac351051b5df97ebbe8a7924ca4
Branislav Stojkovic <Branislav Stojkovic@Bancika-PC>
2016-10-28 00:42:01 +0200

    moved folder

---
+++ java-http-proxy/src/com/diyfever/httpproxy/PhpFlatProxy.java
@@ -0,0 +1,75 @@
+package com.diyfever.httpproxy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+
+/**
+ * {@link IFlatProxy} implementation for PHP. Method names are interpreted as
+ * PHP script names, excluding the ".php" extension. Example below shows how
+ * native Java call is translated into PHP call:<br>
+ * <br>
+ * <code>invoke("yahoo.com", "search", params)</code> <br>
+ * <br>
+ * This call will query the server using PHP script
+ * <code>yahoo.com/search.php</code> and will pass the parameters using POST
+ * method.
+ * 
+ * @author Branislav Stojkovic
+ */
+public class PhpFlatProxy implements IFlatProxy {
+
+	private static final Logger LOG = Logger.getLogger(PhpFlatProxy.class);
+
+	@Override
+	public InputStream invoke(String url, String methodName, Map<String, Object> params) {
+		InputStream serverInput;
+		try {
+			// Flatten params map into an array
+			Object[] paramsArray = new Object[params.size() * 2];
+			int i = 0;
+			for (Map.Entry<String, Object> entry : params.entrySet()) {
+				paramsArray[i++] = entry.getKey();
+				paramsArray[i++] = entry.getValue();
+			}
+
+			// Call the server.
+			URL phpUrl = new java.net.URL(createPhpFileName(url, methodName));
+			LOG.debug("Connecting to: " + phpUrl);
+			serverInput = ClientHttpRequest.post(phpUrl, paramsArray);
+			return serverInput;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public Object invokeAndDeserialize(String url, String methodName, Map<String, Object> params) {
+		InputStream stream = invoke(url, methodName, params);
+		if (stream == null) {
+			return null;
+		}
+		// Deserialize the stream
+		XStream xstream = new XStream(new JettisonMappedXmlDriver());
+		xstream.setMode(XStream.NO_REFERENCES);
+		return xstream.fromXML(stream);
+	}
+
+	private String createPhpFileName(String url, String methodName) {
+		if (url.endsWith("/")) {
+			return url + methodName + ".php";
+		} else {
+			return url + "/" + methodName + ".php";
+		}
+	}
+}
---
+++ java-http-proxy/test/com/diyfever/httpproxy/ProxyFactoryTest.java
@@ -0,0 +1,48 @@
+package com.diyfever.httpproxy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diyfever.httpproxy.ProxyFactory;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+
+public class ProxyFactoryTest {
+
+	private ProxyFactory factory;
+
+	@Before
+	public void setUp() throws Exception {
+		BasicConfigurator.configure();
+		factory = new ProxyFactory(new MockFlatProxy());
+	}
+
+	@Test
+	public void testCreateProxyString() {
+		ITestProxy proxy = factory.createProxy(ITestProxy.class, "doesn't matter");
+		String name = "bancika";
+		assertEquals(name, proxy.test(name));
+	}
+
+	@Test
+	public void testCreateProxyStream() throws IOException {
+		ITestProxy proxy = factory.createProxy(ITestProxy.class, "doesn't matter");
+		String name = "bancika";
+		InputStream stream = proxy.testStream(name);
+		byte[] buff = new byte[256];
+		int length = stream.read(buff);
+		String result = new String(buff, 0, length);
+
+		XStream xstream = new XStream(new JettisonMappedXmlDriver());
+		xstream.setMode(XStream.NO_REFERENCES);
+		String expected = xstream.toXML(name);
+
+		assertEquals(expected, result);
+	}
+}
